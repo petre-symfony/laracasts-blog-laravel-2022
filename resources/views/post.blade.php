2201aecbe06f84c6d46b1c1a197e0d8fcb46c002
petrero <petrero@gmail.com>
2022-12-08 17:51:00 +0200

    8.1 Pass a php variable from the route to post template

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,8 +4,7 @@
 
 <body>
     <article>
-        <h1><a href="/post">My first post</a></h1>
-        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad adipisci alias aliquam aperiam asperiores aspernatur consequuntur corporis cumque eius eveniet fugit illum in iure magnam natus nihil numquam odit quasi repellat sapiente similique soluta sunt, voluptates voluptatibus? Veniam, voluptatibus. </p>
+    <?= $post ?>
     </article>
 
     <a href="/">Go Back</a>
