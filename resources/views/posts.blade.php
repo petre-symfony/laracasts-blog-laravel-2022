370befcc4fb6c16e13595be227487777e58bf8f2
petrero <petrero@gmail.com>
2022-12-08 18:29:17 +0200

    8.4 Update links to individual posts in index page

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,17 +4,17 @@
 
 <body>
     <article>
-        <h1><a href="/post">My first post</a></h1>
+        <h1><a href="/posts/my-first-post">My first post</a></h1>
         <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad adipisci alias aliquam aperiam asperiores aspernatur consequuntur corporis cumque eius eveniet fugit illum in iure magnam natus nihil numquam odit quasi repellat sapiente similique soluta sunt, voluptates voluptatibus? Veniam, voluptatibus. </p>
     </article>
 
     <article>
-        <h1><a href="/post">My second post</a></h1>
+        <h1><a href="/posts/my-second-post">My second post</a></h1>
         <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum nisi quaerat ratione similique voluptatum. Accusantium aspernatur consequatur excepturi molestias neque non obcaecati quidem ullam veniam! </p>
     </article>
 
     <article>
-        <h1><a href="/post">My third post</a></h1>
+        <h1><a href="/posts/my-third-post">My third post</a></h1>
         <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam architecto est eveniet facilis inventore nam nihil nostrum numquam obcaecati placeat praesentium ratione repellendus repudiandae unde ut vel vitae, voluptate voluptatum! </p>
     </article>
 </body>
