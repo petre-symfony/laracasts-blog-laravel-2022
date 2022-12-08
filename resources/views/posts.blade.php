ed588f142a616bd53274bb5d55c07ab2ffe21842
petrero <petrero@gmail.com>
2022-12-08 17:17:45 +0200

    7.1 Craft a blog with posts like page

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1>My first post</h1>
+        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad adipisci alias aliquam aperiam asperiores aspernatur consequuntur corporis cumque eius eveniet fugit illum in iure magnam natus nihil numquam odit quasi repellat sapiente similique soluta sunt, voluptates voluptatibus? Veniam, voluptatibus. </p>
+    </article>
+
+    <article>
+        <h1>My second post</h1>
+        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum nisi quaerat ratione similique voluptatum. Accusantium aspernatur consequatur excepturi molestias neque non obcaecati quidem ullam veniam! </p>
+    </article>
+
+    <article>
+        <h1>My third post</h1>
+        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam architecto est eveniet facilis inventore nam nihil nostrum numquam obcaecati placeat praesentium ratione repellendus repudiandae unde ut vel vitae, voluptate voluptatum! </p>
+    </article>
+</body>
