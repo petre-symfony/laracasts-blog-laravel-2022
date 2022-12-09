4c0b3b78a5af87a7ba9a22901f482847c9c9d5f8
petrero <petrero@gmail.com>
2022-12-09 20:19:16 +0200

    12.9 Move code from the index route to post model

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
-use Illuminate\Support\Facades\File;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,18 +14,8 @@ use Illuminate\Support\Facades\File;
 */
 
 Route::get('/', function () {
-    $posts = collect(File::files(resource_path("posts/")))
-        ->map(fn($file) => YamlFrontMatter::parseFile($file))
-        ->map(fn($document) => new Post(
-            $document->title,
-            $document->excerpt,
-            $document->date,
-            $document->body(),
-            $document->slug
-        ));
-
     return view('posts', [
-        'posts' => $posts
+        'posts' => Post::all()
     ]);
 });
 
