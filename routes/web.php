05f095fbbafade979e419347e0afc48db12b636c
petrero <petrero@gmail.com>
2022-12-09 19:45:33 +0200

    12.8 Upgrade to arrow functions

--- routes/web.php
+++ routes/web.php
@@ -18,18 +18,14 @@ use Illuminate\Support\Facades\File;
 
 Route::get('/', function () {
     $posts = collect(File::files(resource_path("posts/")))
-        ->map(function($file){
-            return YamlFrontMatter::parseFile($file);
-        })
-        ->map(function($document){
-            return new Post(
-                $document->title,
-                $document->excerpt,
-                $document->date,
-                $document->body(),
-                $document->slug
-            );
-        });
+        ->map(fn($file) => YamlFrontMatter::parseFile($file))
+        ->map(fn($document) => new Post(
+            $document->title,
+            $document->excerpt,
+            $document->date,
+            $document->body(),
+            $document->slug
+        ));
 
     return view('posts', [
         'posts' => $posts
