20b45683870154a914cd4390e23a5fb9ecb58deb
petrero <petrero@gmail.com>
2022-12-09 19:27:52 +0200

    12.5 Use array_map instead foreach loop

--- routes/web.php
+++ routes/web.php
@@ -18,18 +18,17 @@ use Illuminate\Support\Facades\File;
 
 Route::get('/', function () {
     $files = File::files(resource_path("posts/"));
-    $posts = [];
 
-    foreach($files as $file) {
+    $posts = array_map(function($file){
         $document = YamlFrontMatter::parseFile($file);
-        $posts[] = new Post(
+        return new Post(
             $document->title,
             $document->excerpt,
             $document->date,
             $document->body(),
             $document->slug
         );
-    }
+    }, $files);
 
     return view('posts', [
         'posts' => $posts
