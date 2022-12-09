54fb8edc11a5537c9643de0607fba1131f0ed074
petrero <petrero@gmail.com>
2022-12-09 18:50:24 +0200

    12.2 Return in index route an array of Post object whose properties are extracted with metadata parsed by spacie objects

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,6 +6,18 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\File;
 
 class Post {
+    public $title;
+    public $excerpt;
+    public $date;
+    public $body;
+
+    public function __construct($title, $excerpt, $date, $body){
+        $this->body = $body;
+        $this->excerpt = $excerpt;
+        $this->date = $date;
+        $this->title = $title;
+    }
+
     public static function find($slug) {
         if (! file_exists($path = resource_path("posts/{$slug}.html"))){
             throw new ModelNotFoundException();
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
+use Illuminate\Support\Facades\File;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,6 +17,20 @@ use App\Models\Post;
 */
 
 Route::get('/', function () {
+    $files = File::files(resource_path("posts/"));
+    $posts = [];
+
+    foreach($files as $file) {
+        $document = YamlFrontMatter::parseFile($file);
+        $posts[] = new Post(
+            $document->title,
+            $document->excerpt,
+            $document->date,
+            $document->body()
+        );
+    }
+    ddd($posts);
+
     return view('posts', [
         'posts' => Post::all()
     ]);
