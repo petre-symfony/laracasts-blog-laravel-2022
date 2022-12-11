96298eb8c38e126a63c597994ec358b31b1b0791
petrero <petrero@gmail.com>
2022-12-11 19:22:55 +0200

    25.2 Bind categories route to slug field of the Category model instead of id

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ Route::get('posts/{post:slug}', function(Post $post){
     ]);
 });
 
-Route::get('categories/{category}', function(Category $category){
+Route::get('categories/{category:slug}', function(Category $category){
     return view('posts', [
         'posts' => $category->posts
     ]);
