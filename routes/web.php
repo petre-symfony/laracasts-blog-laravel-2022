73d08741b140d93f859b8eab3599e076bbefe675
petrero <petrero@gmail.com>
2022-12-11 19:18:34 +0200

    25.1 Show posts related to one category

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
+use App\Models\Category;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -22,5 +23,11 @@ Route::get('/', function () {
 Route::get('posts/{post:slug}', function(Post $post){
     return view('post', [
         'post' => $post
+    ]);
+});
+
+Route::get('categories/{category}', function(Category $category){
+    return view('posts', [
+        'posts' => $category->posts
     ]);
 });
