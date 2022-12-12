5233cbe039924cf5f4a742bd8dc1f05671e52c30
petrero <petrero@gmail.com>
2022-12-12 09:21:02 +0200

    27.1 Alter posts migration table and run php artisan migrate:fresh

--- database/migrations/2022_12_11_103149_create_posts_table.php
+++ database/migrations/2022_12_11_103149_create_posts_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->string('title');
+            $table->foreignId('user_id');
             $table->foreignId('category_id');
             $table->string('slug')->unique();
             $table->text('excerpt');
