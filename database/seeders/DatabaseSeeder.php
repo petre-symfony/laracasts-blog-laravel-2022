2633c0bfcb77907b9572d2a09ec67395cb01e915
petrero <petrero@gmail.com>
2022-12-12 09:28:29 +0200

    27.2 Create three categories in the seeder, make category fields unique and run php artisan migrate:fresh --seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +16,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $user = User::factory()->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Category::create([
+            'name' => 'Family',
+            'slug' => 'family'
+        ]);
+
+        Category::create([
+            'name' => 'Work',
+            'slug' => 'work'
+        ]);
     }
 }
