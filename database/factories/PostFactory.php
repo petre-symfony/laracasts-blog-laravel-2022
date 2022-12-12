b5e1e3360c394a2e6b365cb6deb07027197f4f72
petrero <petrero@gmail.com>
2022-12-12 10:22:23 +0200

    28.2 Fill post factory

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Category;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +19,11 @@ class PostFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'user_id' => User::factory(),
+            'category_id' => Category::factory(),
+            'title' => $this->faker->sentence,
+            'excerpt' => $this->faker->sentence,
+            'body' => $this->faker->paragraph
         ];
     }
 }
