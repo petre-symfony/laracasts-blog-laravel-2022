ca85203e74c2b91d4940faaf480ea7b57b76e7ef
petrero <petrero@gmail.com>
2022-12-12 11:00:15 +0200

    29.4 Create a route that displays the posts created by an author

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
 use App\Models\Category;
+use App\Models\User;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -29,5 +30,11 @@ Route::get('posts/{post:slug}', function(Post $post){
 Route::get('categories/{category:slug}', function(Category $category){
     return view('posts', [
         'posts' => $category->posts
+    ]);
+});
+
+Route::get('authors/{author}', function(User $author){
+    return view('posts', [
+        'posts' => $author->posts
     ]);
 });
