b9e516ac76f5ff7b7e226ec2328b406182c19c50
petrero <petrero@gmail.com>
2022-12-13 12:53:46 +0200

    33.2 Remove duplication for category button

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -36,9 +36,7 @@
                         </a>
 
                         <div class="space-x-2">
-                            <a href="/categories/{{ $post->cattegory->slug }}"
-                               class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
-                               style="font-size: 10px">{{ $post->cattegory->name }}</a>
+                            <x-category-button :category="$post->category" />
                         </div>
                     </div>
 
