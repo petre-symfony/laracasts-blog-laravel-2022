96fb7c1fbf7e88cd65143e9295a11496a4c1bf46
petrero <petrero@gmail.com>
2022-12-13 12:01:49 +0200

    32.9 Put the posts beyond first three on a row

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,9 +6,9 @@
             <x-post-featured-card :post="$posts[0]" />
 
             @if ($posts->count() > 1)
-                <div class="lg:grid lg:grid-cols-2">
+                <div class="lg:grid lg:grid-cols-6">
                     @foreach($posts->skip(1) as $post)
-                        <x-post-card :post="$post" class="bg-red-500"/>
+                        <x-post-card :post="$post" class="col-span-2"/>
                     @endforeach
                 </div>
             @endif
