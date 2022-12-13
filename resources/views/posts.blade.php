ade33244e3a1b40467d2b8552225344b0a8d47eb
petrero <petrero@gmail.com>
2022-12-13 12:09:56 +0200

    32.10 Divide posts two on a row beyond the first and three on a row beyond the third

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -8,7 +8,10 @@
             @if ($posts->count() > 1)
                 <div class="lg:grid lg:grid-cols-6">
                     @foreach($posts->skip(1) as $post)
-                        <x-post-card :post="$post" class="col-span-2"/>
+                        <x-post-card
+                            :post="$post" class="col-span-2"
+                            class="{{ $loop->iteration < 3 ? 'col-span-3' : 'col-span-2' }}"
+                        />
                     @endforeach
                 </div>
             @endif
