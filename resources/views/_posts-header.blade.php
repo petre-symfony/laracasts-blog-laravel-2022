65dd4b20746dd21bf0fc0dcafea63dd61e0dd3d1
petrero <petrero@gmail.com>
2022-12-14 10:56:18 +0200

    34.15 Make options part dynamic. But when click on one option we get an error because $categories are passed only to overview page not to any other one where we are going to

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -33,29 +33,14 @@
                 </button>
 
                 <div x-show="show" class="py-2 absolute bg-gray-100 w-full mt-2 z-50" style="display: none">
-                    <a href="" class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white">One</a>
-                    <a href="" class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white">Two</a>
-                    <a href="" class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white">A Longer Link</a>
+                    @foreach($categories as $category)
+                        <a href="/categories/{{ $category->slug }}" class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white">
+                            {{ ucwords($category->name) }}
+                        </a>
+                    @endforeach
                 </div>
             </div>
-
-            {{--
-            <select class="flex-1 appearance-none bg-transparent py-2 pl-3 pr-9 text-sm font-semibold">
-                <option value="category" disabled selected>Category</option>
-                @foreach($categories as $category)
-                    <option value="{{ $category->slug }}">{{ $category->name }}</option>
-                @endforeach
-            </select>
-
-            <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"
-                 height="22" viewBox="0 0 22 22">
-                <g fill="none" fill-rule="evenodd">
-                    <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
-                    </path>
-                    <path fill="#222"
-                          d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"></path>
-                </g>
-            </svg>--}}
+            
         </div>
 
         <!-- Other Filters -->
