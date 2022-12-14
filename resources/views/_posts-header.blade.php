188437c9f0b29b8b8bb5babcf9713ced3f20f681
petrero <petrero@gmail.com>
2022-12-14 11:44:56 +0200

    35.3 We don't need alpinejs specific code in our partial

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -17,7 +17,6 @@
             <x-dropdown >
                 <x-slot name="trigger">
                     <button
-                        @click="show = !show"
                         class="py-2 pl-3 pr-9 text-sm font-semibold w-full lg:w-32 text-left flex lg:inline-flex"
                     >
                         {{ isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories' }}
@@ -33,7 +32,7 @@
                         </svg>
                     </button>
                 </x-slot>
-                
+
                 <a href="/" class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white">
                     All
                 </a>
@@ -42,10 +41,10 @@
                     <a
                         href="/categories/{{ $category->slug }}"
                         class="block text-left px-3 text-sm leading-6
-                    hover:bg-blue-500 focus:bg-blue-500
-                    hover:text-white focus:text-white
-                    {{ isset($currentCategory) && $currentCategory->is($category) ? 'bg-blue-500 text-white' : '' }}
-                            "
+                            hover:bg-blue-500 focus:bg-blue-500
+                            hover:text-white focus:text-white
+                            {{ isset($currentCategory) && $currentCategory->is($category) ? 'bg-blue-500 text-white' : '' }}
+                        "
                     >
                         {{ ucwords($category->name) }}
                     </a>
