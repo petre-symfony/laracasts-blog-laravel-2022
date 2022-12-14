090c3c5e1bec42c3d89265625f85d12f8006c44d
petrero <petrero@gmail.com>
2022-12-14 10:48:53 +0200

    34.14 Make dropdown options to disappear when click outside their containing div

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -14,7 +14,7 @@
     <div class="space-y-2 lg:space-y-0 lg:space-x-4 mt-8">
         <!--  Category -->
         <div class="relative lg:inline-flex lg:items-center bg-gray-100 rounded-xl">
-            <div x-data="{ show: false }">
+            <div x-data="{ show: false }" @click.away="show = false">
                 <button
                     @click="show = !show"
                     class="py-2 pl-3 pr-9 text-sm font-semibold w-full lg:w-32 text-left flex lg:inline-flex"
