97b924522ff5ed859903867e03885a00de6a64f6
petrero <petrero@gmail.com>
2022-12-14 09:44:06 +0200

    34.5 Avoid to push everything bellow when toggle category options visible

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -17,7 +17,7 @@
             <div x-data="{ show: false }">
                 <button @click="show = !show" class="py-2 pl-3 pr-9 text-sm font-semibold">Categories</button>
 
-                <div x-show="show" class="py-2">
+                <div x-show="show" class="py-2 absolute">
                     <a href="" class="block text-left px-3 text-sm leading-6 hover:bg-gray-300 focus:bg-grauy-300">One</a>
                     <a href="" class="block text-left px-3 text-sm leading-6 hover:bg-gray-300 focus:bg-grauy-300">Two</a>
                     <a href="" class="block text-left px-3 text-sm leading-6 hover:bg-gray-300 focus:bg-grauy-300">Three</a>
