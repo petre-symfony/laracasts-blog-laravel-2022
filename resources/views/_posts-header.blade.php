cf60a9460eaa07d7e12c09152a61e9f594b28d6f
petrero <petrero@gmail.com>
2022-12-14 09:42:19 +0200

    34.4 Style the dropdown div.

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -15,12 +15,12 @@
         <!--  Category -->
         <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">
             <div x-data="{ show: false }">
-                <button @click="show = !show">Categories</button>
+                <button @click="show = !show" class="py-2 pl-3 pr-9 text-sm font-semibold">Categories</button>
 
-                <div x-show="show">
-                    <a href="">One</a>
-                    <a href="">Two</a>
-                    <a href="">Three</a>
+                <div x-show="show" class="py-2">
+                    <a href="" class="block text-left px-3 text-sm leading-6 hover:bg-gray-300 focus:bg-grauy-300">One</a>
+                    <a href="" class="block text-left px-3 text-sm leading-6 hover:bg-gray-300 focus:bg-grauy-300">Two</a>
+                    <a href="" class="block text-left px-3 text-sm leading-6 hover:bg-gray-300 focus:bg-grauy-300">Three</a>
                 </div>
             </div>
 
@@ -30,7 +30,7 @@
                 @foreach($categories as $category)
                     <option value="{{ $category->slug }}">{{ $category->name }}</option>
                 @endforeach
-            </select> --}}
+            </select>
 
             <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"
                  height="22" viewBox="0 0 22 22">
@@ -40,7 +40,7 @@
                     <path fill="#222"
                           d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"></path>
                 </g>
-            </svg>
+            </svg>--}}
         </div>
 
         <!-- Other Filters -->
