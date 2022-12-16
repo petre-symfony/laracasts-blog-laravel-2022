fa7c58aed8ceebb7147d7eb012fa621500e9a77d
petrero <petrero@gmail.com>
2022-12-16 11:58:00 +0200

    38.6 Slightly diffrent version of filtering the posts

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -13,11 +13,12 @@ class Post extends Model
     protected $with = ['category', 'author'];
 
     public function scopeFilter($query, array $filters){
-        if ($filters['search'] ?? false) {
+        $query->when($filters['search'] ?? false, function($query, $search) {
             $query
-                ->where('title', 'like', '%' . request('search') . '%')
-                ->orWhere('body', 'like', '%' . request('search') . '%');
-        }
+                ->where('title', 'like', '%' . $search . '%')
+                ->orWhere('body', 'like', '%' . $search . '%');
+        });
+
     }
 
     public function category(){
