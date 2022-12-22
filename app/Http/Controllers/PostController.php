0df7707e2da4ccbc1d8a23c333c190fbfb0e6ee6
petrero <petrero@gmail.com>
2022-12-22 08:56:19 +0200

    40.3 Display current category in blade component

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,8 +9,8 @@ use Illuminate\Http\Request;
 class PostController extends Controller {
    public function index(){
        return view('posts', [
-           'posts' => Post::latest()->filter(request(['search', 'category']))->get(),
-           'currentCategory' => Category::firstWhere('slug', request('category'))
+           'posts' => Post::latest()->filter(request(['search', 'category']))->get()
+
        ]);
    }
 
--- app/View/Components/CategoryDropdown.php
+++ app/View/Components/CategoryDropdown.php
@@ -22,7 +22,8 @@ class CategoryDropdown extends Component {
      */
     public function render() {
         return view('components.category-dropdown', [
-            'categories' => Category::all()
+            'categories' => Category::all(),
+            'currentCategory' => Category::firstWhere('slug', request('category'))
         ]);
     }
 }
