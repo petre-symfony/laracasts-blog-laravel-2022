0e438ea6196a216c3379ce3b26021abd99599e06
petrero <petrero@gmail.com>
2022-12-22 10:13:48 +0200

    42.2 Preserve search query filter when select a category from category dropdown after we type some search term on overview page

--- resources/views/posts/_header.blade.php
+++ resources/views/posts/_header.blade.php
@@ -42,7 +42,7 @@
                 @if (request('category'))
                     <input type="hidden" name="category" value="{{ request('category') }}">
                 @endif
-                
+
                 <input type="text" name="search" placeholder="Find something"
                        class="bg-transparent placeholder-black font-semibold text-sm"
                        value="{{ request('search') }}"
