533835ff4ae27128d122496b5f1249d5585882eb
petrero <petrero@gmail.com>
2022-12-23 10:29:18 +0200

    45.3 Create post register route and validate the request after the form is submitted

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -8,4 +8,13 @@ class RegisterController extends Controller {
     public function create(){
         return view('register.create');
     }
+
+    public function store(){
+        request()->validate([
+            'name' => 'required|max:255',
+            'username' => 'required|max:255|min:3',
+            'email' => 'required|email|max:255',
+            'password' => 'required|min:7|max:255'
+        ]);
+    }
 }
