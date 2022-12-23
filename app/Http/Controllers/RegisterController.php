f652cad9146672a9ca69d8d63193b9e6eb0f1e77
petrero <petrero@gmail.com>
2022-12-23 12:47:21 +0200

    47.5 Alternative way to write the username validation rules

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class RegisterController extends Controller {
     public function create(){
@@ -15,6 +16,7 @@ class RegisterController extends Controller {
         $attributes = request()->validate([
             'name' => 'required|max:255',
             'username' => 'required|min:3|max:255|unique:users,username',
+            'username' => ['required', 'min:3', 'max:255', Rule::unique('users', 'username')],
             'email' => 'required|email|max:255',
             'password' => 'required|min:7|max:255'
         ]);
