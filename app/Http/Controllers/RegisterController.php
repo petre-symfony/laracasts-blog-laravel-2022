29ed4eb4b96795e35df4f3ef1f57cb023c6a5c40
petrero <petrero@gmail.com>
2022-12-23 10:33:51 +0200

    45.4 Protect against the cross site request forgery attack

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -10,11 +10,12 @@ class RegisterController extends Controller {
     }
 
     public function store(){
-        request()->validate([
+        var_dump(request()->all());
+        /*request()->validate([
             'name' => 'required|max:255',
             'username' => 'required|max:255|min:3',
             'email' => 'required|email|max:255',
             'password' => 'required|min:7|max:255'
-        ]);
+        ]);*/
     }
 }
--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -3,6 +3,8 @@
         <main class="max-w-lg mx-auto mt-10 bg-gray-100 border border-gray-200 p-6 rounded-xl">
             <h1 class="text-center font-bold text-xl">Register!</h1>
             <form action="/register" method="post" class="mt-10">
+                @csrf
+
                 <div class="mb-6">
                     <label for="name" class="block mb-2 uppercase font-bold text-xs text-gray-700">
                         Name
