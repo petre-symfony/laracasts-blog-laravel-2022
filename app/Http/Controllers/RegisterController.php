20a41b1f88a297845a5d3c5a3f4e686e917383e8
petrero <petrero@gmail.com>
2022-12-23 12:53:22 +0200

    47.6 Add validation checks to the bottom of the form

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Validation\Rule;
+//use Illuminate\Validation\Rule;
 
 class RegisterController extends Controller {
     public function create(){
@@ -16,7 +16,7 @@ class RegisterController extends Controller {
         $attributes = request()->validate([
             'name' => 'required|max:255',
             'username' => 'required|min:3|max:255|unique:users,username',
-            'username' => ['required', 'min:3', 'max:255', Rule::unique('users', 'username')],
+            /*'username' => ['required', 'min:3', 'max:255', Rule::unique('users', 'username')],*/
             'email' => 'required|email|max:255',
             'password' => 'required|min:7|max:255'
         ]);
