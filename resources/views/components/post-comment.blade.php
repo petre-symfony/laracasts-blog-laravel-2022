6770b80a273c93027d1b4d1a8920f5fd973d78f0
petrero <petrero@gmail.com>
2022-12-24 16:59:30 +0200

    52.4 Put the comment markup in its own blade component

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,23 @@
+<article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
+    <div class="flex-shrink-0">
+        <img src="https://i.pravatar.cc/100" alt="" width="60" heught="60" class="rounded-xl">
+    </div>
+    <div>
+        <header class="mb-4">
+            <h3 class="font-bold">John Doe</h3>
+            <p class="text-xs">
+                Posted
+                <time>a month ago</time>
+            </p>
+        </header>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Blanditiis corporis dolores, possimus quibusdam veritatis
+            voluptates voluptatum! Aliquam animi aperiam aut, autem,
+            dolores earum harum illo ipsum libero minus molestias nam
+            nesciunt nostrum obcaecati quaerat qui rem tempora vitae
+            voluptate voluptatibus.
+        </p>
+    </div>
+</article>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -51,30 +51,8 @@
                     </div>
                 </div>
 
-                <section class="col-span-8 col-start-5 mt-10">
-                    <article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
-                        <div class="flex-shrink-0">
-                            <img src="https://i.pravatar.cc/100" alt="" width="60" heught="60" class="rounded-xl">
-                        </div>
-                        <div>
-                            <header class="mb-4">
-                                <h3 class="font-bold">John Doe</h3>
-                                <p class="text-xs">
-                                    Posted
-                                    <time>a month ago</time>
-                                </p>
-                            </header>
-
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                                Blanditiis corporis dolores, possimus quibusdam veritatis
-                                voluptates voluptatum! Aliquam animi aperiam aut, autem,
-                                dolores earum harum illo ipsum libero minus molestias nam
-                                nesciunt nostrum obcaecati quaerat qui rem tempora vitae
-                                voluptate voluptatibus.
-                            </p>
-                        </div>
-                    </article>
+                <section class="col-span-8 col-start-5 mt-10 space-y-6">
+                    <x-post-comment />
                 </section>
             </article>
         </main>
