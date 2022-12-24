004a45c608f23a1f98733e9a49ed2d979488f041
petrero <petrero@gmail.com>
2022-12-25 00:03:28 +0200

    54.3 Display comments associated to a post

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,23 +1,20 @@
+@props(['comment'])
+
 <article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
     <div class="flex-shrink-0">
-        <img src="https://i.pravatar.cc/100" alt="" width="60" heught="60" class="rounded-xl">
+        <img src="https://i.pravatar.cc/60?u={{ $comment->id }}" alt="" width="60" heught="60" class="rounded-xl">
     </div>
     <div>
         <header class="mb-4">
-            <h3 class="font-bold">John Doe</h3>
+            <h3 class="font-bold">{{ $comment->author->username }}</h3>
             <p class="text-xs">
                 Posted
-                <time>a month ago</time>
+                <time>{{ $comment->created_at }}</time>
             </p>
         </header>
 
         <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Blanditiis corporis dolores, possimus quibusdam veritatis
-            voluptates voluptatum! Aliquam animi aperiam aut, autem,
-            dolores earum harum illo ipsum libero minus molestias nam
-            nesciunt nostrum obcaecati quaerat qui rem tempora vitae
-            voluptate voluptatibus.
+            {{ $comment->body }}
         </p>
     </div>
 </article>
