09eb0399d32ea8429195b17f92349a538c04896a
petrero <petrero@gmail.com>
2022-12-24 16:55:40 +0200

    52.3 Round the corners of the image

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -54,10 +54,10 @@
                 <section class="col-span-8 col-start-5 mt-10">
                     <article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
                         <div class="flex-shrink-0">
-                            <img src="https://i.pravatar.cc/100" alt="" width="60" heught="60">
+                            <img src="https://i.pravatar.cc/100" alt="" width="60" heught="60" class="rounded-xl">
                         </div>
                         <div>
-                            <header>
+                            <header class="mb-4">
                                 <h3 class="font-bold">John Doe</h3>
                                 <p class="text-xs">
                                     Posted
