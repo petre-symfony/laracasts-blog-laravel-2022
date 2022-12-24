79763c1d5aa4d051a0330b7e136345e8e9636e99
petrero <petrero@gmail.com>
2022-12-24 13:02:55 +0200

    50.1 Create login form and login routes

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class SessionsController extends Controller {
+    public function create(){
+        return view('sessions.create');
+    }
+
     public function destroy(){
         auth()->logout();
 
