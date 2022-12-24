7e0aedb4943c688a0a464e754e09558dbae6e754
petrero <petrero@gmail.com>
2022-12-24 10:40:21 +0200

    48.2 Make the flash message to disappear after 4 seconds

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -56,7 +56,12 @@
     </section>
 
     @if(session()->has('success'))
-        <div class="fixed bg-blue-500 text-white py-2 px-4 rounded-xl bottom-3 right-3 text-sm">
+        <div
+            x-data="{ show: true }"
+            x-show="show"
+            x-init="setTimeout(() => show = false, 4000)"
+            class="fixed bg-blue-500 text-white py-2 px-4 rounded-xl bottom-3 right-3 text-sm"
+        >
             <p>{{ session('success') }}</p>
         </div>
     @endif
