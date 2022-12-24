f863c77ab4d4b955eedd20eea4fec325e843e8ba
petrero <petrero@gmail.com>
2022-12-24 10:33:44 +0200

    48.1 Create a flash message that displays after an user has been created

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -23,6 +23,8 @@ class RegisterController extends Controller {
 
         User::create($attributes);
 
+        session()->flash('success', 'Your account has been created');
+
         return redirect('/');
     }
 }
