18f3293a15e4324f28248734da699353dd4f936e
petrero <petrero@gmail.com>
2022-12-26 09:47:53 +0200

    56.3 Disable all mass assignment restrictions on the model level

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,8 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model {
     use HasFactory;
 
-    protected $guarded = [];
-
     public function post(){
         return $this->belongsTo(Post::class);
     }
