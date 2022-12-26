44c8a3546ff8d16bf3a2a0ae8709ee33993e43cc
petrero <petrero@gmail.com>
2022-12-26 09:42:25 +0200

    56.2 Create a comment

--- app/Http/Controllers/PostCommentsController.php
+++ app/Http/Controllers/PostCommentsController.php
@@ -6,7 +6,16 @@ use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostCommentsController extends Controller {
-    public function store(Post $post){
-        dd(request()->all());
+    public function store(Post $post, Request $request){
+        $request->validate([
+            'body' => 'required'
+        ]);
+
+        $post->comments()->create([
+            'user_id' => $request->user()->id,
+            'body' => $request->input('body')
+        ]);
+
+        return back();
     }
 }
