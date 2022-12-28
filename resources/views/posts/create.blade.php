c6561d6058b3385487f276958ae435ee0af0770f
petrero <petrero@gmail.com>
2022-12-28 08:48:41 +0200

    62.1 Write a page where we'll create a post in the future

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+    <section class="px-6 py-6">
+        Hello World
+    </section>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -36,3 +36,5 @@ Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth'
 
 Route::get('login', [SessionsController::class, 'create'])->middleware('guest');
 Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
+
+Route::get('admin/posts/create', [PostController::class, 'create']);
