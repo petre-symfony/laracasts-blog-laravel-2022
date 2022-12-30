b76ffb0a44b7e22db52de658ff1ead0693c9c6b7
petrero <petrero@gmail.com>
2022-12-30 10:50:47 +0200

    66.18 As consequence following the last change we made type prop is no longer needed. Type will be text by default and we can declare it another type because {{ $attributes }} attribute we put it into input blade component in previous commit

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -1,10 +1,9 @@
-@props(['name', 'type' => 'text'])
+@props(['name'])
 
 <x-form.field>
     <x-form.label name="{{ $name }}"></x-form.label>
 
     <input
-        type="{{ $type }}"
         class="border border-gray-200 p-2 w-full rounded"
         name="{{ $name }}" id="{{ $name }}"
         value="{{ old($name) }}"
