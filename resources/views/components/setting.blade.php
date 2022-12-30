47a6744549947f508ca3fe856779dfdaee6e84e1
petrero <petrero@gmail.com>
2022-12-30 12:35:08 +0200

    67.4 Don't allow the sidebar to shrink

--- resources/views/components/setting.blade.php
+++ resources/views/components/setting.blade.php
@@ -4,7 +4,7 @@
     <h1 class="text-lg font-bold mb-8 pb-2 border-b">{{ $heading }}</h1>
 
     <div class="flex">
-        <aside class="w-48">
+        <aside class="w-48 flex-shrink-0">
             <h4 class="font-semibold mb-4">Links</h4>
             <ul>
                 <li>
