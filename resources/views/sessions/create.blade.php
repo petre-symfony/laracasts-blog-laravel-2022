be7f71409a0aa07514f530b6234f50ec102b2888
petrero <petrero@gmail.com>
2022-12-30 10:45:33 +0200

    66.16 Use panel blade component to style the login form

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -1,17 +1,19 @@
 <x-layout>
     <section class="px-6 py-6">
-        <main class="max-w-lg mx-auto mt-10 bg-gray-100 border border-gray-200 p-6 rounded-xl">
-            <h1 class="text-center font-bold text-xl">Log In</h1>
+        <main class="max-w-lg mx-auto mt-10">
+            <x-panel>
+                <h1 class="text-center font-bold text-xl">Log In</h1>
 
-            <form action="/login" method="post" class="mt-10">
-                @csrf
+                <form action="/login" method="post" class="mt-10">
+                    @csrf
 
-                <x-form.input type="email" name="email"></x-form.input>
+                    <x-form.input type="email" name="email"></x-form.input>
 
-                <x-form.input type="password" name="password"></x-form.input>
+                    <x-form.input type="password" name="password"></x-form.input>
 
-                <x-form.button>Log In</x-form.button>
-            </form>
+                    <x-form.button>Log In</x-form.button>
+                </form>
+            </x-panel>
         </main>
     </section>
 </x-layout>
