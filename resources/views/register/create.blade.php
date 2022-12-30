8be7ec404188b6b3e34a1829d36ae96d6f71eca5
petrero <petrero@gmail.com>
2022-12-30 11:00:45 +0200

    66.20 Use form input blade component in register form

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -1,90 +1,22 @@
 <x-layout>
     <section class="px-6 py-6">
-        <main class="max-w-lg mx-auto mt-10 bg-gray-100 border border-gray-200 p-6 rounded-xl">
-            <h1 class="text-center font-bold text-xl">Register!</h1>
-            <form action="/register" method="post" class="mt-10">
-                @csrf
+        <main class="max-w-lg mx-auto mt-10">
+            <x-panel>
+                <h1 class="text-center font-bold text-xl">Register!</h1>
+                <form action="/register" method="post" class="mt-10">
+                    @csrf
 
-                <div class="mb-6">
-                    <label for="name" class="block mb-2 uppercase font-bold text-xs text-gray-700">
-                        Name
-                    </label>
+                    <x-form.input name="name"></x-form.input>
 
-                    <input
-                        type="text"
-                        class="border border-gray-400 p-2 w-full"
-                        name="name"
-                        id="name"
-                        value="{{ old('name') }}"
-                    >
+                    <x-form.input name="username"></x-form.input>
 
-                    @error('name')
-                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
+                    <x-form.input name="email" type="email" autocomplete="username"></x-form.input>
 
-                <div class="mb-6">
-                    <label for="username" class="block mb-2 uppercase font-bold text-xs text-gray-700">
-                        Username
-                    </label>
+                    <x-form.input name="password" type="password" autocomplete="new-password"></x-form.input>
 
-                    <input
-                        type="text"
-                        class="border border-gray-400 p-2 w-full"
-                        name="username"
-                        id="username"
-                        value="{{ old('username') }}"
-                    >
-
-                    @error('username')
-                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
-
-                <div class="mb-6">
-                    <label for="email" class="block mb-2 uppercase font-bold text-xs text-gray-700">
-                        Email
-                    </label>
-
-                    <input
-                        type="email"
-                        class="border border-gray-400 p-2 w-full"
-                        name="email"
-                        id="email"
-                        value="{{ old('email') }}"
-                    >
-
-                    @error('email')
-                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
-
-                <div class="mb-6">
-                    <label for="password" class="block mb-2 uppercase font-bold text-xs text-gray-700">
-                        Password
-                    </label>
-
-                    <input type="password" class="border border-gray-400 p-2 w-full" name="password" id="password">
-
-                    @error('password')
-                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
-
-                <div class="mb-6">
-                    <button type="submit" class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500">
-                        Submit
-                    </button>
-                </div>
-
-                @if ($errors->any())
-                    <ul>
-                        @foreach($errors->all() as $error)
-                            <li class="text-red-500 text-xs">{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                @endif
-            </form>
+                    <x-form.button>Register</x-form.button>
+                </form>
+            </x-panel>
         </main>
     </section>
 </x-layout>
