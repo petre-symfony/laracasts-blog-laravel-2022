b3e8012365099577e286028ce471ef1a9ea9f54d
petrero <petrero@gmail.com>
2022-12-30 10:40:55 +0200

    66.15 Use the input form blade components for login form

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -6,47 +6,11 @@
             <form action="/login" method="post" class="mt-10">
                 @csrf
 
-                <div class="mb-6">
-                    <label for="email" class="block mb-2 uppercase font-bold text-xs text-gray-700">
-                        Email
-                    </label>
+                <x-form.input type="email" name="email"></x-form.input>
 
-                    <input
-                        type="email"
-                        class="border border-gray-400 p-2 w-full"
-                        name="email"
-                        id="email"
-                        value="{{ old('email') }}"
-                        autocomplete="username"
-                    >
+                <x-form.input type="password" name="password"></x-form.input>
 
-                    @error('email')
-                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
-
-                <div class="mb-6">
-                    <label for="password" class="block mb-2 uppercase font-bold text-xs text-gray-700">
-                        Password
-                    </label>
-
-                    <input
-                        type="password"
-                        class="border border-gray-400 p-2 w-full"
-                        name="password" id="password"
-                        autocomplete="new-password"
-                    >
-
-                    @error('password')
-                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
-
-                <div class="mb-6">
-                    <button type="submit" class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500">
-                        Log In
-                    </button>
-                </div>
+                <x-form.button>Log In</x-form.button>
             </form>
         </main>
     </section>
