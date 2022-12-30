d72db89579f0f869c3a39dcaa97bb112d4a1e670
petrero <petrero@gmail.com>
2022-12-30 10:34:28 +0200

    66.13 Style the form inputs in their blade components

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -5,9 +5,10 @@
 
     <input
         type="{{ $type }}"
-        class="border border-gray-400 p-2 w-full"
+        class="border border-gray-200 p-2 w-full rounded"
         name="{{ $name }}" id="{{ $name }}"
         value="{{ old($name) }}"
+        required
     >
 
     <x-form.error name="{{ $name }}"></x-form.error>
