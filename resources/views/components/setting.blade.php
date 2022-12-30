037de7f55e2484fd568568b9e9dd7fc3d098f739
petrero <petrero@gmail.com>
2022-12-30 10:13:09 +0200

    66.9 Layout the main and aside sections from the left to the right

--- resources/views/components/setting.blade.php
+++ resources/views/components/setting.blade.php
@@ -3,17 +3,19 @@
 <section class="py-6 max-w-md mx-auto">
     <h1 class="text-lg font-bold mb-6">{{ $heading }}</h1>
 
-    <aside>
-        <ul>
-            <li>
-                <a href="/admin/posts/create">New Post</a>
-            </li>
-        </ul>
-    </aside>
-    
-    <main>
-        <x-panel>
-            {{ $slot }}
-        </x-panel>
-    </main>
+    <div class="flex">
+        <aside>
+            <ul>
+                <li>
+                    <a href="/admin/posts/create">New Post</a>
+                </li>
+            </ul>
+        </aside>
+
+        <main>
+            <x-panel>
+                {{ $slot }}
+            </x-panel>
+        </main>
+    </div>
 </section>
