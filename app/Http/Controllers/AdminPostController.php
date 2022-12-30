33383474dd2f571850e74485e294c0104930db6c
petrero <petrero@gmail.com>
2022-12-30 21:48:29 +0200

    67.13 Write the update action. As in regard of the slug uniqueness of this field must be checked on the other posts but not on the current one. Without adding the ignore method the slug will not ever validate. The thumbnail field is not required for editing but must be an image

--- app/Http/Controllers/AdminPostController.php
+++ app/Http/Controllers/AdminPostController.php
@@ -41,4 +41,23 @@ class AdminPostController extends Controller {
             'post' => $post
         ]);
     }
+
+    public function update(Post $post){
+        $attributes = request()->validate([
+            'title' => 'required',
+            'thumbnail' => 'image',
+            'slug' => ['required', Rule::unique('posts', 'slug')->ignore($post->id)],
+            'excerpt' => 'required',
+            'body' => 'required',
+            'category_id' => ['required', Rule::exists('categories', 'id')]
+        ]);
+
+        if (isset($attributes['thumbnail'])) {
+            $attributes['thumbnail'] = request()->file('thumbnail')->store('thumbnails');
+        }
+
+        $post->update($attributes);
+
+        return back()->with('success', 'Post updated');
+    }
 }
