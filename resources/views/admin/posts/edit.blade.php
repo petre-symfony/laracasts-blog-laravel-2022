9e7d7007917ae26ebb5deb7d7fc5ffd141aaa275
petrero <petrero@gmail.com>
2022-12-30 21:20:51 +0200

    67.10 Prepopulate the form with the actual values of the post

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -4,15 +4,15 @@
         <form action="/admin/posts" method="post" enctype="multipart/form-data">
             @csrf
 
-            <x-form.input name="title"></x-form.input>
+            <x-form.input name="title" :value="old('title', $post->title)"></x-form.input>
 
-            <x-form.input name="slug"></x-form.input>
+            <x-form.input name="slug" :value="old('slug', $post->slug)"></x-form.input>
 
-            <x-form.input name="thumbnail" type="file"></x-form.input>
+            <x-form.input name="thumbnail" type="file" :value="old('thumbnail', $post->thumbnail)"></x-form.input>
 
-            <x-form.textarea name="excerpt"></x-form.textarea>
+            <x-form.textarea name="excerpt">{{ old('excerpt', $post->excerpt) }}</x-form.textarea>
 
-            <x-form.textarea name="body"></x-form.textarea>
+            <x-form.textarea name="body">{{ old('body', $post->body) }}</x-form.textarea>
 
             <x-form.field>
                 <x-form.label name="category"></x-form.label>
@@ -25,7 +25,7 @@
                     @foreach($categories as $category)
                         <option
                             value="{{ $category->id }}"
-                            {{ old('category_id') == $category->id ? 'selected' : ''}}
+                            {{ old('category_id', $post->category_id) == $category->id ? 'selected' : ''}}
                         >{{ ucwords($category->name) }}</option>
                     @endforeach
                 </select>
