c147a7c03d90546aa92cf8455589e3d8d8cd8d79
petrero <petrero@gmail.com>
2022-12-30 10:11:18 +0200

    66.8 Structure the sublayout previously created

--- resources/views/components/setting.blade.php
+++ resources/views/components/setting.blade.php
@@ -3,7 +3,17 @@
 <section class="py-6 max-w-md mx-auto">
     <h1 class="text-lg font-bold mb-6">{{ $heading }}</h1>
 
-    <x-panel>
-        {{ $slot }}
-    </x-panel>
+    <aside>
+        <ul>
+            <li>
+                <a href="/admin/posts/create">New Post</a>
+            </li>
+        </ul>
+    </aside>
+    
+    <main>
+        <x-panel>
+            {{ $slot }}
+        </x-panel>
+    </main>
 </section>
