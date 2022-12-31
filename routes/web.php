198332aafe04dc046926897e4a615ac2c56e3b79
petrero <petrero@gmail.com>
2022-12-31 13:10:25 +0200

    69.8 Group all admin routes

--- routes/web.php
+++ routes/web.php
@@ -38,9 +38,14 @@ Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth'
 Route::get('login', [SessionsController::class, 'create'])->middleware('guest');
 Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
 
-Route::get('admin/posts', [AdminPostController::class, 'index'])->middleware('can:admin');
-Route::get('admin/posts/{post}/edit', [AdminPostController::class, 'edit'])->middleware('can:admin');
-Route::get('admin/posts/create', [AdminPostController::class, 'create'])->middleware('can:admin');
-Route::post('admin/posts', [AdminPostController::class, 'store'])->middleware('admin');
-Route::patch('admin/posts/{post}', [AdminPostController::class, 'update'])->middleware('can:admin');
-Route::delete('admin/posts/{post}', [AdminPostController::class, 'destroy'])->middleware('can:admin');
+//group admin
+Route::middleware('can:admin')->group(function(){
+    Route::get('admin/posts', [AdminPostController::class, 'index'])->middleware('can:admin');
+    Route::get('admin/posts/{post}/edit', [AdminPostController::class, 'edit'])->middleware('can:admin');
+    Route::get('admin/posts/create', [AdminPostController::class, 'create'])->middleware('can:admin');
+    Route::post('admin/posts', [AdminPostController::class, 'store'])->middleware('admin');
+    Route::patch('admin/posts/{post}', [AdminPostController::class, 'update'])->middleware('can:admin');
+    Route::delete('admin/posts/{post}', [AdminPostController::class, 'destroy'])->middleware('can:admin');
+});
+
+
